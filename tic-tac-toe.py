9acb43f02cc88127729542ac7711e7dff6f737f6
Tony Le <103531472+TonyLeCode@users.noreply.github.com>
2023-12-31 13:50:02 -0800

    removed comment

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -12,43 +12,6 @@ from collections import namedtuple, deque
 from itertools import count
 
 
-# Transition = namedtuple('Transition',
-#                         ('state', 'action', 'next_state', 'reward'))
-
-# class ReplayMemory(object):
-
-#     def __init__(self, capacity):
-#         self.memory = deque([], maxlen=capacity)
-
-#     def push(self, *args):
-#         """Save a transition"""
-#         self.memory.append(Transition(*args))
-
-#     def sample(self, batch_size):
-#         return random.sample(self.memory, batch_size)
-
-#     def __len__(self):
-#         return len(self.memory)
-
-# class DQN(nn.Module):
-#   def __init__(self, n_observations, n_actions):
-#     super(DQN, self).__init__()
-#     self.layer1 = nn.Linear(n_observations, 128)
-    
-#   def forward(self, x):
-#     x = F.relu(self.layer1(x))
-#     x = F.relu(self.layer2(x))
-#     return self.layer3(x)
-
-# BATCH_SIZE = 128
-# GAMMA = 0.99
-# EPS_START = 0.9
-# EPS_END = 0.05
-# EPS_DECAY = 1000
-# TAU = 0.005
-# LR = 1e-4
-
-
 # Define the neural network
 class TicTacToeNet(torch.nn.Module):
     def __init__(self):
